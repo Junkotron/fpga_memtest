f408add6010d2cd9780ac028e762dd5787619e9a
junkotron <devetdu@example.com>
2024-02-19 17:23:56 +0000

    fixed build

--- ice40hx8k/build.sh
+++ ice40hx8k/build.sh
@@ -3,17 +3,11 @@
 set -e
 
 
-TOP=sramtest
-NAME=sramtest
-PACKAGE=vq100
-SRCS="../src/sramtest/sramtest.v ../src/sramtest/tristate8.v"
-DEVICE=1k
-
-#TOP=tristatetest
-#NAME=tristatetest
-#PACKAGE=ct256
-#SRCS="../src/tristatetest/tristatetest.v ../src/tristatetest/tristate.v"
-#DEVICE=8k
+TOP=tristatetest
+NAME=tristatetest
+PACKAGE=ct256
+SRCS="../src/tristatetest/tristatetest.v ../src/tristatetest/tristate.v"
+DEVICE=8k
 
 yosys -q -f "verilog -Duse_sb_io" -l ${NAME}.log -p "synth_ice40 -top ${TOP} -abc2 -blif ${NAME}.blif" ${SRCS}
 arachne-pnr -d ${DEVICE} -P ${PACKAGE} -p ${NAME}_${PACKAGE}.pcf ${NAME}.blif -o ${NAME}.txt
